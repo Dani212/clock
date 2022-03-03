a703fe2c0dd52ad492ac4b9036aaadb2f3af6f8a
Dani212 <danielunwana@gmail.com>
2022-03-03 12:40:00 +0000

    added calFooterHeight function

--- src/utils/clock.ts
+++ src/utils/clock.ts
@@ -1,3 +1,4 @@
+import { height } from 'consts';
 import moment from 'moment-timezone';
 
 export const formatedTimezoneDiff = (timezone: string) => {
@@ -5,7 +6,7 @@ export const formatedTimezoneDiff = (timezone: string) => {
 	const localTimeZone = new Date().getTimezoneOffset();
 
 	const lessOrAhead = gmt > -1;
-	const has30Mins = gmt % 1 === 0.5;
+	const has30Mins = Math.abs(gmt % 1) === 0.5;
 
 	if (gmt === localTimeZone) {
 		return 'local time';
@@ -13,10 +14,24 @@ export const formatedTimezoneDiff = (timezone: string) => {
 
 	if (has30Mins) {
 		return `${
-			Math.abs(gmt) > 1 ? Math.trunc(gmt) + ' hrs' : Math.abs(gmt) + ' hr'
+			Math.abs(gmt) > 1
+				? Math.trunc(Math.abs(gmt)) + ' hrs'
+				: Math.abs(gmt) + ' hr'
 		} 30 mins ${lessOrAhead ? 'ahead' : 'behined'}`;
 	}
 	return `${
 		Math.abs(gmt) > 1 ? Math.abs(gmt) + ' hours' : Math.abs(gmt) + ' hour'
 	} ${lessOrAhead ? 'ahead' : 'behined'}`;
+};
+
+export const calFooterHeight = (len: number) => {
+	const dividerNum = 708;
+	if (len === 0) return dividerNum;
+	if (len === 1) return dividerNum - 118;
+	if (len === 2) return dividerNum - 118 * 2;
+	if (len === 3) return dividerNum - 125 * 3;
+	if (len === 4) return dividerNum - 125 * 4;
+	if (len === 5) return dividerNum - 129 * 5;
+
+	if (len > 6) return height * 0.03;
 };
